ccf17fe60c801a3844d07d32485ef5144ea5d6e7
shawnlihst <309236878@qq.com>
2024-09-10 17:34:54 +0800

    修改：     update.sh

--- update.sh
+++ update.sh
@@ -10,18 +10,43 @@ DoCopy=0
 if [ ! -d "git_source" ]; then
     echo "git_source 文件夹不存在，正在创建并初始化..."
     mkdir git_source
-    cd git_source
+    cd git_source || exit 1  # 如果cd失败，则退出脚本
+
     git init
+    if [ $? -ne 0 ]; then
+        echo "git init 失败，终止操作。"
+        exit 1
+    fi
+
     git remote add origin https://github.com/shawnlihst/NIJIJOJI-Profile-Storage.git
+    if [ $? -ne 0 ]; then
+        echo "git remote add 失败，终止操作。"
+        exit 1
+    fi
+
     git pull origin main
+    if [ $? -ne 0 ]; then
+        echo "git pull 失败，终止操作。"
+        exit 1
+    fi
     DoCopy=1
 else
     echo "git_source 文件夹已存在，正在检查更新..."
-    cd git_source
+    cd git_source || exit 1  # 如果cd失败，则退出脚本
+
     git fetch origin main
+    if [ $? -ne 0 ]; then
+        echo "git fetch 失败，终止操作。"
+        exit 1
+    fi
+
     if ! git diff --quiet --exit-code master origin/main; then
         echo "有更新，正在拉取最新代码..."
         git merge FETCH_HEAD
+        if [ $? -ne 0 ]; then
+            echo "git merge 失败，终止操作。"
+            exit 1
+        fi
         DoCopy=1
     else
         echo "已是最新版本，无需更新。"
